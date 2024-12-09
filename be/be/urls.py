89a6128d21f25524c8a6653e521d1c493d650263
Dominik Horut <xhorut01@vutbr.cz>
2024-12-09 14:14:15 +0100

    another try to deploy

--- be/be/urls.py
+++ be/be/urls.py
@@ -1,23 +1,20 @@
-"""
-URL configuration for be project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render
+
+# This view will serve index.html from the static folder
+def serve_index(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', serve_index),  # Serve the index.html file as the root URL
 ]
+
+# In development, serve static files from the static folder
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
