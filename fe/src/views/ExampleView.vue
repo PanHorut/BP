93068265e9cce69f6581c49deb64f13971f0408d
Dominik Horut <xhorut01@vutbr.cz>
2024-12-22 23:40:32 +0100

    img fix 1

--- fe/src/views/ExampleView.vue
+++ fe/src/views/ExampleView.vue
@@ -16,6 +16,19 @@ const topics = ref([]);
 const isCorrect = ref(false);
 const showIcon = ref(false);
 
+const images = ref({});
+
+const preloadImages = () => {
+  const correct = new Image();
+  const wrong = new Image();
+
+  correct.src = correctIcon;
+  wrong.src = wrongIcon;
+
+  images.value.correct = correct;
+  images.value.wrong = wrong;
+};
+
 
 const displayNext = async (data) => {
 
@@ -49,20 +62,28 @@ const fetchExamples = async (selectedIds) => {
  
 };
 
-const displayIcon = (correct) => {
+const displayIcon = async (correct) => {
   console.log(correct);
-  isCorrect.value = correct; 
 
-  nextTick(() => {
-    showIcon.value = true; 
+  isCorrect.value = correct;
 
-    setTimeout(() => {
-      showIcon.value = false;
-    }, 400);
-  });
+  await nextTick();
+
+  const icon = correct
+    ? images.value.correct.src
+    : images.value.wrong.src;
+
+  showIcon.value = true;
+
+  setTimeout(() => {
+    showIcon.value = false; 
+  }, 400);
 };
 
+
 onMounted(() => {
+  preloadImages();
+
   if (route.query.topics) {
     topics.value = JSON.parse(route.query.topics);
     fetchExamples(topics.value);
@@ -77,7 +98,8 @@ onMounted(() => {
     </div>
     <div class="flex items-center justify-center">
       <Example v-if="examples.length > curr_index" :example="examples[curr_index]" :answer="examples[curr_index].answers[0].answer" @answerSent="displayNext" :key="curr_index"></Example>
-      <img v-if="examples.length > curr_index" :src="isCorrect ? correctIcon : wrongIcon" class="w-48 h-48 absolute t-50" :class="showIcon ? '' : 'hidden'" >
+      <img v-if="examples.length > curr_index"   :src="isCorrect ? images.correct.src : images.wrong.src" 
+      class="w-48 h-48 absolute t-50" :class="showIcon ? '' : 'hidden'" >
       <div v-else class="flex justify-center">
         <RouterLink to="/"  class="text-center text-4xl mt-20 border-4 border-blue-600 p-5 rounded-3xl cursor-pointer">Zpět na hlavní stránku</RouterLink>
       </div>
