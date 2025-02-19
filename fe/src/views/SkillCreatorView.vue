6f5de1e34ad1b3f8b6f0640eba349615223f2461
Dominik Horut <xhorut01@vutbr.cz>
2025-02-19 11:05:55 +0100

    skillcreator spinner

--- fe/src/views/SkillCreatorView.vue
+++ fe/src/views/SkillCreatorView.vue
@@ -2,14 +2,18 @@
 import { ref, onMounted } from 'vue';
 import { getSkillTree } from '@/api/apiClient'; 
 import SkillNode from '@/components/SkillCreator/SkillNode.vue';
+import Spinner from '@/components/Spinner.vue';
 
 const skillTree = ref([]); 
+const loading = ref(true);
 
 onMounted(async () => {
   try {
     skillTree.value = await getSkillTree(); 
   } catch (error) {
     console.error('Error fetching parent skills:', error);
+  } finally{
+    loading.value = false;
   }
 });
 </script>
@@ -19,6 +23,8 @@ onMounted(async () => {
     <h2 class="text-4xl font-bold text-primary my-4 text-center">Dovednosti</h2>
 
     <SkillNode v-if="skillTree.length" :skills="skillTree" :parent="0" />
+    <Spinner v-if="loading" />
+
   </div>
 </template>
 
